c7a88a0c8dd2ed183c318556bc6b862574c6cd74
JJ Aucouturier <aucouturier@gmail.com>
2015-03-20 12:32:30 +0100

    First

--- sethares.py
+++ sethares.py
@@ -1,13 +1,26 @@
-from pylab import plot, show, title, xlabel, ylabel, subplot, savefig, grid, specgram, suptitle, yscale
-from scipy import fft, arange, ifft, io
-from numpy import sin, linspace, pi, cos, sin, random, array
+import numpy as np
+#import matplotlib.pyplot as pp
+
+#from scipy import fft, arange, ifft, io
+#from numpy import sin, linspace, pi, cos, sin, random, array
 #import numpy as np
 #from scipy.io.wavfile import read, write
-from matplotlib import pyplot
 
-def plotCosAndSin():
-	X = linspace(-pi, pi, 256, endpoint=True)
-	C, S = cos(X), sin(X)
-	plot(X, C)
-	plot(X, S)
-	show()
+def  tonal_dissonance(f1, f2, v1=1, v2=1):
+    """ 
+    implements Sethares's parameterization of Plomb&Levelt
+    dissonance curves from "Local Consonance and the
+    relationship between timbre and scale", Jasa 1993.
+    inputs:
+        f1,f2: frequency of partial in Hz
+        v1,v2: amplitude of partial           
+    """
+    a = 3.5
+    b = 5.75
+    d_star = 0.24
+    s1 = 0.021 # perhaps 0.21 (CMJ)
+    s2 = 19 
+    s = d_star / (s1*min(f1,f2) + s2) 
+    f_diff = abs(f2-f1) 
+    diss = v1*v2*(np.exp(-a*s*f_diff) - np.exp(-b*s*f_diff))
+    return diss 
